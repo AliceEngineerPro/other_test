70a150cd9376ed58cf571a8ccbd2669e89f10558
AliceEngineer <alice_engineer@yeah.net>
2022-10-10 11:32:54 +0800

    update

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ def write_file():
     if not os.path.exists(os.path.join(os.getcwd(), 'testing')):
         os.makedirs('testing')
 
-    for count in range(100):
+    for count in range(300):
         file_name = f'testing/python_file_{count}.py'
         with open(file=file_name, mode='w+', encoding='utf-8') as file:
             for _ in range(1000000):
-                if not os.path.getsize(filename=file_name) > 1024 * 3:
+                if not os.path.getsize(filename=file_name) > 1024 * 120:
                     file.write('print("I Love Python")\n')
                 else:
                     break
@@ -29,3 +29,5 @@ def write_file():
 
 if __name__ == '__main__':
     write_file()
+
+    print(os.path.getsize('testing/python_file_1.py'), 1024 * 120)
