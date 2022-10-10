d4162b90f82a501e10eebdff7e6552835f46d26b
Tinger <devil.home@foxmail.com>
2022-10-10 10:35:38 +0800

    update

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import os
 
 def write_file():
     
-    for count in range(50):
+    for count in range(100000):
         file_name = f'testing/python_file_{count}.py'
         with open(file=file_name, mode='w+', encoding='utf-8') as file:
             for _ in range(1000000):
-                if not os.path.getsize(filename=file_name) > 1024 ** 2:
+                if not os.path.getsize(filename=file_name) > 1024 * 3:
                     file.write('print("I Love Python")\n')
                 else:
                     break
